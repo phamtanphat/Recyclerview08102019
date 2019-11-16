6c569f4a915a3a1924a312cbed05ed02f89d5470
phatdroid94 <phatdroid94@gmail.com>
2019-11-16 20:25:09 +0700

    gan layout

--- app/src/main/java/com/example/recyclerview08102019/MainActivity.java
+++ app/src/main/java/com/example/recyclerview08102019/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.recyclerview08102019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    MonanAdapter monanAdapter;
+    ArrayList<Monan> monanArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+
+        monanArrayList = new ArrayList<>();
+
+        monanArrayList.add(new Monan("Buzza Pizza - Nguyễn Trung Trực", "Tầng 3, 5-7-9 Nguyễn Trung Trực, P. Bến Thành, Quận 1, TP. HCM", R.drawable.monan_buzza_pizza));
+        monanArrayList.add(new Monan("Morico - Contemporary Japanese Lifestyle - Lê Lợi", "30 Lê Lợi, P. Bến Nghé, Quận 1, TP. HCM", R.drawable.monan_morico));
+        monanArrayList.add(new Monan("Paris Baguette - Cao Thắng", "1 Cao Thắng, P. 2, Quận 3, TP. HCM", R.drawable.monan_paris_baguette));
+        monanArrayList.add(new Monan("San Fu Lou - Ẩm Thực Quảng Đông - Lê Lai", "Tầng Trệt, AB Tower, 76A Lê Lai, Quận 1, TP. HCM", R.drawable.monan_sanfulou));
+        monanArrayList.add(new Monan("Monkey In Black Cafe - Sư Vạn Hạnh", "698 Sư Vạn Hạnh, Quận 10, TP. HCM", R.drawable.monan_tradao));
+
+        monanAdapter = new MonanAdapter(monanArrayList);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(monanAdapter);
+
+
     }
 }
